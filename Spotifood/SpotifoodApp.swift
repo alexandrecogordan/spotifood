a3b705b20ad23964b903b69a24c81dc1a7877193
Alexandre Cogordan <alexcogordan2001@hotmail.fr>
2023-03-12 13:58:18 +0100

    Initial Commit

---
+++ Spotifood/SpotifoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifoodApp.swift
+//  Spotifood
+//
+//  Created by Alexandre Cogordan on 12/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpotifoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
